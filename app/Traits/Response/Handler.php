aa6cb1d46799f71b9e8c2f6d2f767c63e3f07e81
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2024-01-28 18:08:16 +0700

    adjust response 2

--- app/Traits/Response/Handler.php
+++ app/Traits/Response/Handler.php
@@ -8,25 +8,20 @@ trait Handler
 {
     public function response($data = null, bool $status = true, ?string $message = 'success', ?int $code = 200)
     {
-        if ($status === true) {
-            $response = [
-                'code' => $code,
-                'success' => $status,
-                'message' => $message,
-                'data' => $data
-            ];
+        $response = [
+            'code' => $code,
+            'success' => $status,
+            'message' => $message
+        ];
 
+        if ($status === true) {
+            $response['data'] = $data;
             if (getType($data) == "object" && $data->collection) {
                 $response['data'] = $data->resolve()[0];
                 $response['paginate'] = $data->resolve()[1];
             }
         } else {
-            $response = [
-                'code' => $code,
-                'success' => $status,
-                'message' => $message,
-                'errors' => $data,
-            ];
+            $response['errors'] = $data;
         }
 
         return response()->json($response);
